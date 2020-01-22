7d0708977a9b125b81569d58b11070718130e37e
szoupi <szoupi@gmail.com>
2020-01-23 00:07:31 +0200

    add cards

--- burnout-results.php
+++ burnout-results.php
@@ -19,28 +19,28 @@ include 'assets/templates/header.html';
 
 
         if ($results <= 18) {
-            $class = "alert-success";
-            $heading =  "ΚΑΝΕΝΑ ΣΗΜΑΔΙ ";
+            $class = "success";
+            $header =  "ΚΑΝΕΝΑ ΣΗΜΑΔΙ ";
             $content = "xxx";
 
         } elseif ($results >= 19 && $results <= 32 ) {
-            $class = "alert-info";
-            $heading = "ΙΣΩΣ ΣΟΒΑΡΟΙ ΠΑΡΑΓΟΝΤΕΣ";
+            $class = "info";
+            $header = "ΙΣΩΣ ΣΟΒΑΡΟΙ ΠΑΡΑΓΟΝΤΕΣ";
             $content = "xxx";
 
         } elseif ($results >= 33 && $results <= 49 ) {
-            $class = "alert-primary";
-            $heading = "ΠΡΙΝ ΤΟ BURNOUT";
+            $class = "primary";
+            $header = "ΠΡΙΝ ΤΟ BURNOUT";
             $content = "xxx";
 
         } elseif ($results >= 50 && $results <= 59 ) {
-            $class = "alert-warning";
-            $heading = "ΣΟΒΑΡΟΣ ΚΙΝΔΥΝΟΣ BURNOUT";
+            $class = "warning";
+            $header = "ΣΟΒΑΡΟΣ ΚΙΝΔΥΝΟΣ BURNOUT";
             $content = "xxx";
 
         } elseif ($results >= 60) {
-            $class = "alert-danger";
-            $heading = "BURNOUT";
+            $class = "danger";
+            $header = "BURNOUT";
             $content = "xxx";
         }
         
@@ -53,16 +53,27 @@ include 'assets/templates/header.html';
 <div class="container ">
     <div class="row p-1 m-1">
         <div class="col-md-8 offset-md-2">
-			
-			<h1>Εργασιακό κάψιμο - burnout (τεστ)</h1>
-			<small class="text-muted">BURN-OUT /ˈb3ːrnaʊt/US:/ˈbɝnˌaʊt/ ,(bûrn′out′):<br />Αναλώνομαι προοδευτικά εκ των ένδον μέχρι του σημείου της απανθράκωσης</small>
-			<br /><br />
+            
+            <h1>Εργασιακό κάψιμο - burnout (τεστ)</h1>
+            <small class="text-muted">BURN-OUT /ˈb3ːrnaʊt/US:/ˈbɝnˌaʊt/ ,(bûrn′out′):<br />Αναλώνομαι προοδευτικά εκ των ένδον μέχρι του σημείου της απανθράκωσης</small>
+            <br /><br />
+
+
+                <div class="card text-white bg-<?php echo $class; ?> mb-3" style="max-width: 22rem;">
+                    <div class="card-header"><?php echo $header; ?></div>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $results; ?>/75</h5>
+                        <p class="card-text"><?php echo $content; ?></p>
+                    </div>
+                </div>        
+
+
 			
 			<div class="alert <?php echo $class; ?>">
 				<h4>
 					Η Βαθμολογία σου είναι: <?php echo $results; ?>/75
 				</h4>
-                <h3 class="alert-heading" ><?php echo $heading; ?></h3>
+                <h3 class="alert-heading" ><?php echo $header; ?></h3>
                 <p><?php echo $content; ?></p>
 			</div>
                 
